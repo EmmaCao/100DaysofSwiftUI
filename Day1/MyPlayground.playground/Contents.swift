077692c442e727d54f51f87e7981a443f0f9cdeb
Emma <emma.caoqin@qq.com>
2019-12-05 09:46:40 +0800

    day1

---
+++ Day1/MyPlayground.playground/Contents.swift
@@ -0,0 +1,35 @@
+import UIKit
+
+var str = "Hello, playground"
+
+str = "Goodbye"
+
+var age = 38
+
+var population = 8_000_000
+
+var str1 = """
+This goes \
+over multiple \
+lines
+"""
+
+var pi = 3.141
+
+pi = 4
+
+print(pi)
+
+var awesome = true
+
+var score = 85
+str = "Your score was \(score)"
+var result = "The test result are here: \(str)"
+
+let taylor = "swift"
+
+let album: String = "Reputation"
+let year: Int = 1989
+let height: Double = 1.78
+let taylorRocks: Bool = true
+
