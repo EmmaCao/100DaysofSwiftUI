5a6be3dfd5a91174cefa604393a9c651ebb60ddc
Emma <emma.caoqin@qq.com>
2019-12-10 11:22:40 +0800

    day8

---
+++ Day8/Day8.playground/Contents.swift
@@ -0,0 +1,89 @@
+import UIKit
+
+//1. Creating your own structs
+//2. Computed properties
+
+struct Sport {
+    var name: String
+    var isOlympicSport: Bool
+    
+    var olympicStatus: String {
+        if isOlympicSport {
+            return "\(name) is an Olympic sport"
+        } else {
+            return "\(name) is not an Olympic sport"
+        }
+    }
+}
+
+//var tennis = Sport(name: "Tennis")
+//print(tennis.name)
+//
+//tennis.name = "Lawn tennis"
+
+let chessBoxing = Sport(name: "Chessboxing", isOlympicSport: false)
+print(chessBoxing.olympicStatus)
+
+
+//3. Property observers
+
+struct Progress {
+    var task: String
+    var amount: Int {
+        didSet {
+            print("\(task) is now \(amount)% complete")
+        }
+    }
+}
+
+var progress = Progress(task: "Loading data", amount: 0)
+progress.amount = 30
+progress.amount = 80
+progress.amount = 100
+
+//4. Methods
+struct City {
+    var population: Int
+    
+    func collectTaxes() -> Int {
+        return population * 1000
+    }
+}
+
+let london = City(population: 9_000_000)
+london.collectTaxes()
+
+//5. Mutating methods
+
+struct Person {
+    var name: String
+    
+    mutating func makeAnonymous() {
+        name = "Anonymous"
+    }
+}
+
+var person = Person(name: "Ed")
+print(person.name)
+person.makeAnonymous()
+print(person.name)
+
+//5. Properties and methods of strings
+
+let string = "Do or do not, there is no try."
+print(string.count)
+print(string.hasPrefix("Do"))
+print(string.uppercased())
+print(string.sorted())
+
+//6. Properties and methods of arrays
+
+var toys = ["Woody"]
+print(toys.count)
+toys.append("Buzz")
+toys.firstIndex(of: "Buzz")
+print(toys.sorted())
+toys.remove(at: 0)
+
+var examScores = [100,95,92]
+examScores.insert(98, at: 0)
