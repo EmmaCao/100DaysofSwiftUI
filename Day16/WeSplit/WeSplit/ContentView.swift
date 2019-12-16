5971de92d6d6dce5d1ef3eeb23b569421be711dc
Emma <emma.caoqin@qq.com>
2019-12-16 13:56:00 +0800

    Day16, my first SwiftUI project.

---
+++ Day16/WeSplit/WeSplit/ContentView.swift
@@ -0,0 +1,31 @@
+//
+//  ContentView.swift
+//  WeSplit
+//
+//  Created by 曹琴 on 2019/12/16.
+//  Copyright © 2019 csike. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    let students = ["Harry", "Hermione", "Ron"]
+    @State private var selectedStudent = 0
+    
+    var body: some View {
+        VStack {
+            Picker("Select your student", selection: $selectedStudent) {
+                ForEach(0..<students.count) {
+                    Text(self.students[$0])
+                }
+            }
+            Text("You chose: Student # \(students[selectedStudent])")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
