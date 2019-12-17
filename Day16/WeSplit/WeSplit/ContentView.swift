1e7373e42e7fff5089882ad7268e4f67a4bc2039
Emma <emma.caoqin@qq.com>
2019-12-17 13:56:25 +0800

    Day17, finished the challenge of WeSplit.

--- Day16/WeSplit/WeSplit/ContentView.swift
+++ Day16/WeSplit/WeSplit/ContentView.swift
@@ -9,18 +9,74 @@
 import SwiftUI
 
 struct ContentView: View {
-    let students = ["Harry", "Hermione", "Ron"]
-    @State private var selectedStudent = 0
+    @State private var checkAmount = ""
+    @State private var numberOfPeople = ""
+    @State private var tipPercentage = 2
+    let tipPercentages = [10, 15, 20, 25, 0]
+    
+    var numberOfPeopleCnt: Double {
+        return Double(numberOfPeople) ?? 0
+    }
+    
+    var peopleCount: Double {
+        return Double(numberOfPeopleCnt + 2)
+    }
+    
+    var tipSelection: Double {
+        return Double(tipPercentages[tipPercentage])
+    }
+    
+    var orderAmount: Double {
+        return Double(checkAmount) ?? 0
+    }
+    
+    var tipValue: Double {
+        return (orderAmount / 100 * tipSelection)
+    }
+    
+    var grandTotal: Double {
+        return (orderAmount + tipValue)
+    }
+    
+    var totalPerPerson: Double {
+        return (grandTotal / peopleCount)
+    }
     
     var body: some View {
-        VStack {
-            Picker("Select your student", selection: $selectedStudent) {
-                ForEach(0..<students.count) {
-                    Text(self.students[$0])
+        NavigationView {
+            Form {
+                Section {
+                    TextField("Amount", text: $checkAmount)
+//                        .keyboardType(.decimalPad)
+                    
+//                    Picker("Number of people", selection: $numberOfPeople) {
+//                        ForEach(2..<100) {
+//                            Text("\($0) people")
+//                        }
+//                    }
+                    TextField("Number of people, default is 2", text: $numberOfPeople)
+                        .keyboardType(.decimalPad)
                 }
-            }
-            Text("You chose: Student # \(students[selectedStudent])")
+                
+                Section(header: Text("Total Amount")) {
+                    Text("Total Amount \(grandTotal, specifier: "%.2f")")
+                }
+                
+                Section(header: Text("Amount per Person")) {
+                    Text("$\(totalPerPerson, specifier: "%.2f")")
+                }
+                
+                Section(header:Text("How much tip do you want to leave?")) {
+                    
+                    Picker("Tip percentage", selection: $tipPercentage) {
+                        ForEach(0..<tipPercentages.count) {
+                            Text("\(self.tipPercentages[$0])%")
+                        }
+                    }
+                }.pickerStyle(SegmentedPickerStyle())
+            }.navigationBarTitle("WeSplit")
         }
+
     }
 }
 
