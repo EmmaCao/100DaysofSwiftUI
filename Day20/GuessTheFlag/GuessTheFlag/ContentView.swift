fb365a27f8705e56722968c94191f0b859f18314
Emma <emma.caoqin@qq.com>
2020-01-09 10:04:23 +0800

    Day34, new challenge of GuessTheFlag

--- Day20/GuessTheFlag/GuessTheFlag/ContentView.swift
+++ Day20/GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -11,55 +11,90 @@ import SwiftUI
 struct ContentView: View {
     @State private var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"].shuffled()
     @State private var correctAnswer = Int.random(in: 0...2)
+    
     @State private var showingScore = false
     @State private var scoreTitle = ""
-    @State private var score = 0
+    @State private var currentScore = 0
+    @State private var showAnimation = false
+    
+    @State private var selectedAnswer = 0
     
     var body: some View {
         ZStack {
-            LinearGradient(gradient: Gradient(colors: [.blue, .black]), startPoint: .top, endPoint: .bottom)
+            LinearGradient(gradient: Gradient(colors: [.init(.systemBackground), .init(.blue)]), startPoint: .top, endPoint: .bottom)
                 .edgesIgnoringSafeArea(.all)
+                .blur(radius: 20)
             
-            VStack(spacing: 30) {
+            VStack(spacing: 50) {
                 VStack {
                     Text("Tap the flag of")
-                        .foregroundColor(.white)
+                        .font(.subheadline)
+                    
                     Text(countries[correctAnswer])
+                        .font(.largeTitle)
+                        .fontWeight(.black)
+                        .frame(width: 300)
+                }.padding(.top, 20)
+                
+                ForEach(0..<3) { number in
+                    Button(action: {
+                        self.showAnimation = true
+                        self.flagTapped(number)
+                    }){
+                        FlagImage(name: self.countries[number])
+                            .overlay(Color.red.opacity(self.showAnimation && number != self.correctAnswer ? 0.8 : 0)
+                                .cornerRadius(10))
+                            .opacity(self.showAnimation && number != self.selectedAnswer ? 0.2 : 1)
+                            .rotation3DEffect(self.showAnimation && number == self.selectedAnswer ? .degrees(360) : .degrees(0), axis: self.correctAnswer == self.selectedAnswer ? (x:1, y:1, z:1) : (x:0, y:1, z:0))
+                            .scaleEffect(self.showAnimation && number == self.selectedAnswer ? 1.7 : 1)
+                            .animation(Animation.interpolatingSpring(stiffness: 7, damping: 3)
+                                .speed(5)
+                                .delay(0.2)
+                            )
+                    }
+                }
+                
+                VStack {
+                    Text("Current score:")
+                        .foregroundColor(.white)
+                        .font(.subheadline)
+                    Text("\(currentScore)")
                         .foregroundColor(.white)
                         .font(.largeTitle)
                         .fontWeight(.black)
                 }
                 
-                ForEach(0..<3) { number in
-                    Button(action: {
-                        self.flagTapped(number)
-                    }){
-                        FlagImage(country: self.countries[number])
-                    }
-                }
-                Text("\(score)")
-                    .foregroundColor(.white)
-                    .font(.largeTitle)
-                    .fontWeight(.black)
-                
                 Spacer()
             }
-        }.alert(isPresented: $showingScore) {
-            Alert(title: Text(scoreTitle), message: Text("Your score is \(score)"), dismissButton: .default(Text("Continue")){
+        }
+        .alert(isPresented: $showingScore) {
+            Alert(title: Text(scoreTitle), message: Text("That's the flag of \(self.countries[selectedAnswer])"), dismissButton: .default(Text("Continue")){
                 self.askQuestion()
+                self.showAnimation = false
                 })
         }
     }
     
     func flagTapped(_ number: Int) {
         if number == correctAnswer {
+            currentScore += 1
             scoreTitle = "Correct"
-            score += 1
         } else {
-            scoreTitle = "Wrong! That's the flag of \(countries[number])"
+            currentScore -= 1
+            scoreTitle = "Wrong!"
         }
+        selectedAnswer = number
         
-        showingScore = true
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1.5) {
+            if self.selectedAnswer == self.correctAnswer {
+                self.showAnimation = false
+                DispatchQueue.main.asyncAfter(deadline: .now() + 0.33) {
+                    self.askQuestion()
+                }
+            } else {
+                self.showingScore = true
+            }
+        }
     }
     
     func askQuestion() {
@@ -68,20 +103,13 @@ struct ContentView: View {
     }
 }
 
-struct FlagImage: View {
-    var country: String
-    
-    var body: some View {
-        Image(country)
-            .renderingMode(.original)
-            .clipShape(Capsule())
-            .overlay(Capsule().stroke(Color.black, lineWidth: 1))
-            .shadow(color: .black, radius: 2)
-    }
-}
-
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        Group {
+            ContentView()
+                .environment(\.colorScheme, .light)
+            ContentView()
+                .environment(\.colorScheme, .dark)
+        }
     }
 }
--- Day20/GuessTheFlag/GuessTheFlag/FlagImage.swift
+++ Day20/GuessTheFlag/GuessTheFlag/FlagImage.swift
@@ -9,13 +9,34 @@
 import SwiftUI
 
 struct FlagImage: View {
+    
+    let name: String
+    
+    @State private var faded = false
+    @State private var correctlyGuessed = false
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        return Image(name)
+            .renderingMode(.original)
+            .clipShape(RoundedRectangle(cornerRadius: 10))
+            .overlay(RoundedRectangle(cornerRadius: 10).stroke(Color.init(.tertiarySystemBackground), lineWidth: 1))
+            .shadow(color: .primary, radius: 5)
+            .opacity(faded ? 0.2 : 1)
+            .rotation3DEffect(correctlyGuessed ? .degrees(180) : .degrees(0), axis:(x:0, y: 1, z: 0))
+            .animation(.default)
+    }
+    
+    func setCorrectlyGuessed() {
+        self.correctlyGuessed = true
+    }
+    
+    func setFaded() {
+        self.faded = true
     }
 }
 
 struct FlagImage_Previews: PreviewProvider {
     static var previews: some View {
-        FlagImage()
+        FlagImage(name: "Germany")
     }
 }
